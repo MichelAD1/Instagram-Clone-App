7a8a7e1710793e97d08b4460fd09cd64434a098e
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-13 14:04:32 +0200

    Creating explore page

--- Frontend/functions.js
+++ Frontend/functions.js
@@ -7,17 +7,40 @@ let splashScreen = () => {
 let loadHome = () => {
   let home_icon = document.getElementById("home_icon");
   home_icon.src = "logos/homefill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
+  document.getElementById("explore_icon").onclick = function () {
+    window.location.href = "../Frontend/explore.html";
+  };
+  document.getElementById("add_icon").onclick = function () {
+    window.location.href = "../Frontend/addPostStory.html";
+  };
+  document.getElementById("messenger_icon").onclick = function () {
+    window.location.href = "../Frontend/messenger.html";
+  };
+  document.getElementById("acc_icon").onclick = function () {
+    window.location.href = "../Frontend/account.html";
+  };
   let searchh = document.querySelector(".search");
   searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 1;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 0;
+  let title = document.getElementById("title");
+  title.innerHTML = "Instagram";
+};
+let loadExplore = () => {
+  let explore_icon = document.getElementById("explore_icon");
+  explore_icon.src = "logos/explorefill.png";
+  document.getElementById("home_icon").onclick = function () {
+    window.location.href = "../Frontend/home.html";
+  };
+  document.getElementById("add_icon").onclick = function () {
+    window.location.href = "../Frontend/addPostStory.html";
+  };
+  document.getElementById("messenger_icon").onclick = function () {
+    window.location.href = "../Frontend/messenger.html";
+  };
+  document.getElementById("acc_icon").onclick = function () {
+    window.location.href = "../Frontend/account.html";
+  };
+  let searchh = document.querySelector(".search");
+  searchh.style.display = "none";
   let title = document.getElementById("title");
   title.innerHTML = "Instagram";
 };
@@ -32,86 +55,21 @@ document.getElementById("search_icon").onclick = function () {
   search.style.display = "block";
   let searchh = document.querySelector(".search");
   searchh.style.display = "block";
+  document.getElementById("home_icon").onclick = function () {
+    window.location.href = "../Frontend/home.html";
+  };
+  document.getElementById("explore_icon").onclick = function () {
+    window.location.href = "../Frontend/explore.html";
+  };
+  document.getElementById("add_icon").onclick = function () {
+    window.location.href = "../Frontend/addPostStory.html";
+  };
+  document.getElementById("messenger_icon").onclick = function () {
+    window.location.href = "../Frontend/messenger.html";
+  };
+  document.getElementById("acc_icon").onclick = function () {
+    window.location.href = "../Frontend/account.html";
+  };
   let title = document.getElementById("title");
   title.innerHTML = "Instagram";
-};
-document.getElementById("home_icon").onclick = function () {
-  this.src = "logos/homefill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 1;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 0;
-  let title = document.getElementById("title");
-  title.innerHTML = "Instagram";
-};
-document.getElementById("add_icon").onclick = function () {
-  this.src = "logos/addfill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("home_icon").src = "logos/home.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 0;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 0;
-  let title = document.getElementById("title");
-  title.innerHTML = "Create Post";
-};
-document.getElementById("messenger_icon").onclick = function () {
-  this.src = "logos/messengerfill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("home_icon").src = "logos/home.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 0;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 0;
-  let title = document.getElementById("title");
-  title.innerHTML = "Inbox - Chats";
-};
-document.getElementById("explore_icon").onclick = function () {
-  this.src = "logos/explorefill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("home_icon").src = "logos/home.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 0;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 1;
-  let title = document.getElementById("title");
-  title.innerHTML = "Instagram";
-};
-document.getElementById("acc_icon").onclick = function () {
-  this.src = "logos/accountfill.png";
-  document.getElementById("search_icon").src = "logos/search.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("home_icon").src = "logos/home.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "none";
-  let home = document.querySelector(".main-home");
-  home.style.opacity = 0;
-  let explore = document.querySelector(".explore");
-  explore.style.opacity = 0;
-  let title = document.getElementById("title");
-  title.innerHTML = "Michel @michelabidaoud";
 };
