3a575b46304c881f2be8e246135271d43cdd10cf
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-15 17:55:43 +0200

    Creating api for login and signup with jwt tokens

--- Backend/instagram-server/app/Models/User.php
+++ Backend/instagram-server/app/Models/User.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
+    protected $fillable = [
+        'username',
+        'full_name',
+        'password',
+        'bio',
+        'profile_picture'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'username_verified_at' => 'datetime',
+    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     use HasFactory;
 }
--- Backend/instagram-server/routes/api.php
+++ Backend/instagram-server/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::group(["prefix" => "v0.1"], function() {
+    Route::group(["prefix" => "users"], function() {
+        Route::post("login",[AuthController::class,"login"]);
+        Route::post("signup",[AuthController::class,"register"]);
+    });
 	
 });
