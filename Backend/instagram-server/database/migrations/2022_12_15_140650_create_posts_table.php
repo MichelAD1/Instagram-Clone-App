fa8cb9c8e44f65c80f7f5673cc53df40b82e2823
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-15 16:12:53 +0200

    Creating posts table

---
+++ Backend/instagram-server/database/migrations/2022_12_15_140650_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer("posted_by");
+            $table->integer("count_likes");
+            $table->integer("count_comments");
+            $table->string("post_image");
+            $table->string("caption");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
