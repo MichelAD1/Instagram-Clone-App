4d22bd148069861d38086641aa52ef21d920ac8e
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-15 19:19:02 +0200

    Fixing search bar

--- Frontend/functions.js
+++ Frontend/functions.js
@@ -294,30 +294,22 @@ let loadAdd = () => {
   let title = document.getElementById("title");
   title.innerHTML = "Create Post/Story";
 };
-document.getElementById("search_icon").onclick = function () {
-  this.src = "logos/searchfill.png";
-  document.getElementById("home_icon").src = "logos/home.png";
-  document.getElementById("add_icon").src = "logos/add.png";
-  document.getElementById("messenger_icon").src = "logos/messenger.png";
-  document.getElementById("explore_icon").src = "logos/explore.png";
-  document.getElementById("acc_icon").src = "logos/account.png";
-  let search = document.getElementById("search");
-  search.style.display = "block";
-  let searchh = document.querySelector(".search");
-  searchh.style.display = "block";
-  document.getElementById("home_icon").onclick = function () {
-    window.location.href = "../Frontend/home.html";
-  };
-  document.getElementById("explore_icon").onclick = function () {
-    window.location.href = "../Frontend/explore.html";
-  };
-  document.getElementById("add_icon").onclick = function () {
-    window.location.href = "../Frontend/addPostStory.html";
-  };
-  document.getElementById("messenger_icon").onclick = function () {
-    window.location.href = "../Frontend/messenger.html";
-  };
-  document.getElementById("acc_icon").onclick = function () {
-    window.location.href = "../Frontend/account.html";
-  };
+
+let search = () => {
+  let search = document.getElementById("search_icon").src;
+  let name = search.split("/").pop();
+  search = document.getElementById("search_icon");
+  if (name === "search.png") {
+    search.src = "logos/searchfill.png";
+    search = document.getElementById("search");
+    search.style.display = "block";
+    let searchh = document.querySelector(".search");
+    searchh.style.display = "block";
+  } else {
+    search.src = "logos/search.png";
+    search = document.getElementById("search");
+    search.style.display = "none";
+    let searchh = document.querySelector(".search");
+    searchh.style.display = "none";
+  }
 };
